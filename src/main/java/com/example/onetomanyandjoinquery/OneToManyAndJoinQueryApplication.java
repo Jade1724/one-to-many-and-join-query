94f0c2e3a5d5aba88b588c8b90576a16180e8b3c
Haruka Ichinose <hic21@uclive.ac.nz>
2022-01-10 18:20:50 +1300

    Initial commit

---
+++ src/main/java/com/example/onetomanyandjoinquery/OneToManyAndJoinQueryApplication.java
@@ -0,0 +1,13 @@
+package com.example.onetomanyandjoinquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneToManyAndJoinQueryApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OneToManyAndJoinQueryApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/onetomanyandjoinquery/OneToManyAndJoinQueryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onetomanyandjoinquery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneToManyAndJoinQueryApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
