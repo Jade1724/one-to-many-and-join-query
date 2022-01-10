601bf8f8f47d14a5953e85abcc819b25deb9d859
Haruka Ichinose <hic21@uclive.ac.nz>
2022-01-11 12:33:49 +1300

    Added price field and a constructor in OrderResponse.

--- src/main/java/com/example/onetomanyandjoinquery/dto/OrderResponse.java
+++ src/main/java/com/example/onetomanyandjoinquery/dto/OrderResponse.java
@@ -1,5 +1,8 @@
 package com.example.onetomanyandjoinquery.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +12,18 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(Include.NON_DEFAULT)
 public class OrderResponse {
 
   private String name;
   private String productName;
+
+  public OrderResponse(String name, String productName) {
+    this.name = name;
+    this.productName = productName;
+  }
+
+  private int price;
 
 }
